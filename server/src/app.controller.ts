5b11b7454ef2bdbc3e6c1ebc64976a90f2ae782a
eyalgromzin <eyalgromzin@gmail.com>
2026-01-06 10:33:12 +0200

    add nest server

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getRoot(): string {
+  getHello(): string {
     return this.appService.getHello();
   }
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,8 @@
-import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-  await app.listen(port);
-  // eslint-disable-next-line no-console
-  console.log(`Server running on http://localhost:${port}`);
+  await app.listen(process.env.PORT ?? 3000);
 }
-
 bootstrap();
